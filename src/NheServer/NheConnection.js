bc6de2dd6448a8ff9d0de4943a0a74078d96f8a7
Anderson Tavares <acmt@ecomp.poli.br>
2011-06-17 11:58:22 -0300

    Putting conditions
    Change-Id: Ia429769914006b72717c761409afeea1a1c52883
    Signed-off-by: Anderson Tavares <acmt@ecomp.poli.br>

--- src/NheServer/NheConnection.js
+++ src/NheServer/NheConnection.js
@@ -9,6 +9,7 @@
 
 // Importing the Database Package
 var db = require('mysql-native');
+var EventEmitter = require('events').EventEmitter();
 
 /**
  * Constructor for Connection class
@@ -32,15 +33,60 @@ function NheConnection(socketTmp)
 	this.getUsuarioAtual = function(){return usuario_atual;};
 	this.setUsuarioAtual = function(vro_usuario_atual){usuario_atual = vro_usuario_atual;};
 	
-	this.adicionarProjeto = function(id){};
+	this.adicionarProjeto = function(pro_identificador){opened_projects.push(pro_identificador);};
 }
+NheConnection.prototype = new EventEmitter();
 
 NheConnection.prototype.runCommand = function(command, parameters)
 {
-	
+	switch(command)
+	{
+		case '1':// Run Procedure
+			break;
+		case '3':// Send Chat
+			break;
+		case '3':// Move Project
+			break;
+		case '4':// Add Component
+			break;
+		case '5':// Remove Component
+			break;
+		case '6':// Move Component
+			break;
+		case '7':// Undo Project
+			break;
+		case '8':// Reject Owner
+			break;
+		case '9':// Accept Owner
+			break;
+		case '10':// Set Current State of the Project
+			break;
+		case '11':// Finish Project
+			break;
+		case '12':// Ban User
+			break;
+		case '13':// Set role of the user
+			break;
+		case '14':// User connected to the system
+			break;
+		case '15':// User disconnected to the system
+			break;
+		
+	}
 };
 
 NheConnection.prototype.runProcedure = function(procedure, parameters)
 {
 	
 };
+
+NheConnection.prototype.enterProject = function()
+{
+	
+};
+
+NheConnection.prototype.exitProject = function()
+{
+	
+};
+
