161f40a32d08c1b22bf270fa77c95fa19d6f5cf3
Anderson Tavares <nocturne.pe@gmail.com>
2011-06-26 02:43:51 -0300

    Transformando Servidor NodeJS em Red5 (RTMP) e Cumulus (RTMFP)

---
+++ src/NheServer/Red5/WEB-INF/src/br/poli/ecomp/geav/nhe/controller/server/Application.java
@@ -0,0 +1,104 @@
+package br.poli.ecomp.geav.nhe.controller.server;
+
+import org.red5.server.adapter.ApplicationAdapter;
+
+public class Application extends ApplicationAdapter
+{
+	
+	public Boolean login()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean logout()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean listRooms()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean enterRoom()
+	{
+		// TODO: fazer a entrada da sala
+		return true;
+	}
+	
+	public Boolean exitRoom()
+	{
+		// TODO: fazer a saída da sala
+		return true;
+	}
+	
+	public Boolean listParticipants()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean changeUserName()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean changeUserRole()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean loadProject()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean addModule()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean moveModule()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean removeModule()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean undoState()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean setCurrentState()
+	{
+		// TODO: fazer a remoção do módulo
+		return true;
+	}
+	
+	public Boolean removeState()
+	{
+		
+	}
+	
+	public Boolean simulateModel()
+	{
+		
+	}
+	
+}
+
+
